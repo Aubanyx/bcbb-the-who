95d5a4b904c8f284fabd69ecfb0cd34245579ca6
Bastien <bastienlafalize@gmail.com>
2020-11-20 09:18:11 +0100

    footer header

---
+++ includes/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tracker</title>
+    <!-- <link rel="stylesheet" href="/bcbb-the-who/assets/bootstrap/bootstrap.min.css"> -->
+    <link rel="stylesheet" href="/bcbb-the-who/assets/css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js"></script>
+</head>
+<body>
+    <header class="header">
+
+        <div class="header__Title header__Flex">
+            <h1 class="header__Title1">TRACKER</h1>
+        </div>
+
+        <div class="header__button header__Flex">
+            <button type="button" class="header__button1"><i class="far fa-clipboard"></i>  Register</button>
+            <button type="button" class="header__button2"><i class="far fa-arrow-alt-circle-right"></i>  Login</button>
+        </div>
+
+    </header>
