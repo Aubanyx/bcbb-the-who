37736e1717b0b88af705d72b97be3fa5c3a8b227
SandrineLe <le.sandrinemt@gmail.com>
2020-11-23 13:52:32 +0100

    maj/add FRONTEND ALL

--- includes/header.php
+++ includes/header.php
@@ -6,24 +6,28 @@
     <title>Tracker</title>
     <!-- <link rel="stylesheet" href="/bcbb-the-who/assets/bootstrap/bootstrap.min.css"> -->
 
-    <link rel="stylesheet" href="/assets/css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link href="/assets/fontawesome/css/all.css" rel="stylesheet">
-    <link rel="stylesheet" href="/assets/bootstrap/bootstrap.bundle.min.js">
+    <link href="/bcbb-the-who/assets/fontawesome/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" href="/bcbb-the-who/assets/bootstrap/bootstrap.bundle.min.js">
+    <link rel="stylesheet" href="/bcbb-the-who/assets/css/style.css">
 
 
 
 </head>
 <body class="bg-light">
-    <header class="header shadow-sm">
+    
+<header class="header">
 
-        <div class="header__Title header__Flex">
-            <h1 class="header__Title1">TRACKER</h1>
+    <nav class="navbar navbar-expand-lg navbar-light justify-content-end p-5">
+        <div class="" id="navbarNavAltMarkup">
+            <div class=" h4 navbar-nav">
+                <a class="nav-item nav-link active text-light mx-3" href="http://localhost/bcbb-the-who/index.php#"><i class="fas fa-home"></i> Home <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link text-light mx-3" href="http://localhost/bcbb-the-who/pages/register.php"><i class="far fa-arrow-alt-circle-right"></i> Register</a>
+                <a class="nav-item nav-link text-light mx-3" href="http://localhost/bcbb-the-who/pages/login.php"><i class="far fa-clipboard"></i> Login</a>
+            </div>
         </div>
+    </nav>
 
-        <div class="header__button header__Flex">
-        <button type="button" class="header__button1"><a href="http://localhost:8888/pages/register.php"><i class="far fa-clipboard"></i>  Register</a></button>
-            <button type="button" class="header__button2"><a href="http://localhost:8888/pages/login.php"><i class="far fa-arrow-alt-circle-right"></i>  Login</a></button>
-        </div>
+    <h1 class="text-center titre__Header align-items-center">TRACKER</h1>
 
     </header>
